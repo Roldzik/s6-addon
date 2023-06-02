a841221c57dda3c2e8a3251c4b81a3d0cd38734a
Roldzik <135129794+Roldzik@users.noreply.github.com>
2023-06-02 20:54:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,25 +9,27 @@ function czasDoWydarzenia(rok, miesiac, dzien, godzina, minuta, sekunda, milisek
 		var s = pozostalyCzas / 1000;	// sekundy
 		var min = s / 60;				// minuty
 		var h = min / 60;				// godziny
+		var dni = h / 24;
 
 		var sLeft = Math.floor(s  % 60);	// pozostało sekund		
 		var minLeft = Math.floor(min % 60);	// pozostało minut
-		var hLeft = Math.floor(h);			// pozostało godzin	
+		var hLeft = Math.floor(h % 24);			// pozostało godzin	
+		var dniLeft = Math.floor(dni % 30);
 		
 		if (minLeft < 10)
 		  minLeft = "0" + minLeft;
 		if (sLeft < 10)
 		  sLeft = "0" + sLeft;
 		
-		return hLeft + "g : " + minLeft + "m : " + sLeft + "s";
+		return dniLeft + "d : " + hLeft + "g : " + minLeft + "m : " + sLeft + "s";
 	}
 	else
-		return "0g : 0m : 0s";
+		return "0d : 0g : 0m : 0s";
 }
 					
 window.onload = function()
 {
 	idElement = "minutnik";
-	document.getElementById(idElement).innerHTML = czasDoWydarzenia(2023, 05, 02, 20, 18, 0, 0);
-	setInterval("document.getElementById(idElement).innerHTML = czasDoWydarzenia(2023, 05, 02, 20, 18, 0, 0)", 1000);
+	document.getElementById(idElement).innerHTML = czasDoWydarzenia(2023, 05, 01, 20, 0, 0, 0);
+	setInterval("document.getElementById(idElement).innerHTML = czasDoWydarzenia(2023, 05, 01, 20, 0, 0, 0)", 1000);
 };
